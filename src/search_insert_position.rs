7cb24d77e50fd3c104231d5a557636a12439e1b0
Gabriele D'Arrigo <darrigo.g@gmail.com>
2024-01-23 20:26:10 +0100

    Implemented the solution to the search insert position problem

--- src/search_insert_position.rs
+++ src/search_insert_position.rs
@@ -47,8 +47,9 @@ mod tests {
 
     #[test]
     fn test_search_insert() {
-        assert_eq!(Solution::search_insert(vec![1, 3, 5, 6], 0), 2);
+        assert_eq!(Solution::search_insert(vec![1, 3, 5, 6], 5), 2);
         assert_eq!(Solution::search_insert(vec![1, 3, 5, 6], 2), 1);
         assert_eq!(Solution::search_insert(vec![1, 3, 5, 6], 7), 4);
+        assert_eq!(Solution::search_insert(vec![1, 3, 5, 6], 0), 0);
     }
 }
